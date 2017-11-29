709f26c89e6dc20837c128060734d15a54544699
nikhilaggarwal0711 <nikhilaggarwal0711@gmail.com>
2017-11-29 20:54:00 +0530

    bittrex changes.

--- cryptoTelegramBot_TEST/cryptoTelegramBot_TEST/fetchBittrex.py
+++ cryptoTelegramBot_TEST/cryptoTelegramBot_TEST/fetchBittrex.py
@@ -2,18 +2,25 @@ import time
 import requests
 import json
 from dbhelper import  DBHelper
+from time import sleep
 
 class fetchBittrex:
     def __init__(self):
+        print "inside Bittrex constructor"
         self.link1 = "https://bittrex.com/api/v1.1/public/getmarketsummaries"
 
     def setFetchTime(self):
+        print "setFetchTime -- Bittrex"
         self.fetchTime = int(time.time())
 
     def delTillFetchTime(self):
-        self.delTillFetchTime = self.fetchTime - 172800
+        print "delTillFetchTime -- Bittrex"
+#        deleteTime = 172800
+        deleteTime = 1
+        self.delTillFetchTime = self.fetchTime - deleteTime
 
     def fetchData(self):
+        print "fetchData -- Bittrex"
         self.f1 = requests.get(url = self.link1)
         self.data = self.f1.text.replace("null","0")
         self.jsonList  = json.loads(self.data)
@@ -33,9 +40,28 @@ class fetchBittrex:
             self.PrevDay = self.jsonList["result"][x]["PrevDay"]
             self.Created = self.jsonList["result"][x]["Created"].encode('utf-8')
                 
+            self.saveIntoDB()
+            
     def saveIntoDB(self):            
-        DBHelper.addBittrex(self.id,self.name,self.symbol,self.rank,self.price_usd,self.price_btc,self.h24_volume_usd,self.market_cap_usd,self.available_supply,self.total_supply,self.percent_change_1h,self.percent_change_24h,self.percent_change_7d,self.last_updated,self.fetchTime)
+        self.db.addBittrex(self.id,self.name,self.symbol,self.rank,self.price_usd,self.price_btc,self.h24_volume_usd,self.market_cap_usd,self.available_supply,self.total_supply,self.percent_change_1h,self.percent_change_24h,self.percent_change_7d,self.last_updated,self.fetchTime)
 
     def deleteFromDB_fetchTime(self):
-        DBHelper.deleteFromDB_fetchTime("bittrex",self.delTillFetchTime())
+        print "deleteFromDB_fetchTime -- Bittrex"
+        self.db.deleteFromDB_fetchTime("bittrex",self.delTillFetchTime)
     
+    def start(self,sleepTime,db):
+        print "Start method -- Bittrex"
+        self.sleepTime = sleepTime
+        self.db = db
+        try:
+            while True:
+                try:
+                    self.setFetchTime()
+                    self.fetchData()
+                    self.setDelTillFetchTime()
+                    self.deleteFromDB_fetchTime()
+                except:
+                    print "exception caught in while loop -- Bittrex"
+                sleep(self.sleepTime)
+        except:
+            print "Exception caught in start function -- Bittrex"
--- cryptoTelegramBot_TEST/cryptoTelegramBot_TEST/myThread.py
+++ cryptoTelegramBot_TEST/cryptoTelegramBot_TEST/myThread.py
@@ -15,12 +15,13 @@ class MyThread(Thread):
     def run(self):
         print "Starting run method of MyThread"
         if ( self.threadName == "coinMarketCap" ):
-            fcmc = fetchCoinMarketCap()
-            fcmc.start(self.sleepTime, self.db)
+            print "coinmarketcap"
+            #fcmc = fetchCoinMarketCap()
+            #fcmc.start(self.sleepTime, self.db)
         elif  ( self.threadName == "bittrex" ):
             print "bittrex"
-#            fb = fetchBittrex()
-#            fb.start(self.sleepTime, self.db)
+            fb = fetchBittrex()
+            fb.start(self.sleepTime, self.db)
         elif ( self.threadName == "telegram" ):
             print "telegram"
             #rt = runTelegram()
