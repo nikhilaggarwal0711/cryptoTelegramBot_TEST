c8f5758b0c559afcd7a0c383e8513711eb2f5738
nikhilaggarwal0711 <nikhilaggarwal0711@gmail.com>
2017-11-29 22:13:11 +0530

    .

--- cryptoTelegramBot_TEST/cryptoTelegramBot_TEST/runTelegram.py
+++ cryptoTelegramBot_TEST/cryptoTelegramBot_TEST/runTelegram.py
@@ -6,12 +6,14 @@ from time import sleep
 
 class RunTelegram:
     def __init__(self):
+        print "Inside telegram constructor -- Telegram"
         self.TOKEN = TELEGRAM.token
         self.TelegramBot = telepot.Bot(self.TOKEN)
         self.db = DBHelper()
         self.db.setup()
         
     def setup_pythonAnyWhere(self):
+        print "Inside setup method -- Telegram" 
         proxy_url = TELEGRAM.proxy_url
         telepot.api._pools = {
                 'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=60),
@@ -19,12 +21,15 @@ class RunTelegram:
         telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=60))
         
     def getLastOffset(self):
+        print "Inside getLastOffset -- Telegram"
         self.lastOffset = self.db.getLastOffset()
 
     def getUpdates(self):
+        print "Inside getUpdates -- Telegram"
         self.updates = self.TelegramBot.getUpdates(int(self.lastOffset)+1)
 
     def fetchData(self,update):
+        print "Inside fetchData -- "
         self.text = update["message"]["text"]
         self.chatId = update["message"]["from"]["id"]
         self.offsetId = update["update_id"]
@@ -32,15 +37,19 @@ class RunTelegram:
 
     #Following function will return either new or old
     def checkUser(self):
+        print "Inside checkUser -- Telegram"
         self.user = self.db.checkUser(self.chatId)
 
     def newUser(self):
+        print "Inside newUser -- Telegram"
         self.message = "I have added you in my notification list. \nFuture Upgrades : \n1. More Exchanges \n2. Provide Rank of newly added market \n3. Price Alerts \n4. Portfolio Tracker\n5. FREE Money/Coins alerts"
 
     def addBotMessage(self):
+        print "Inside addBotMessage -- Telegram"
         self.db.addBotMessage(self.lastOffset,self.chatId , self.firstName, "g" , self.offsetId, self.fetchTime)
         
     def handleUpdate(self):
+        print "Inside handleUpdate -- Telegram"
         if self.text == "/start" or self.text == "start":
             self.message="I know its too long since any new market is added, but I am tracking and will keep you posted. Thanks for poking :) "
         else:
@@ -48,6 +57,7 @@ class RunTelegram:
 
 
     def sendTelegramMessage(self):
+        print "Inside sendTelegramMessage -- Telegram"
         self.TelegramBot.sendMessage(self.chatId,self.message)
 
     def start(self,sleepTime):
