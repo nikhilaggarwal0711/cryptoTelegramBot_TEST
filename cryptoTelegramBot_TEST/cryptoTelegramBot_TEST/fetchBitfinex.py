8f53dc1f1616c8716737b1080fc833464ce06a24
nikhilaggarwal0711 <nikhilaggarwal0711@gmail.com>
2017-12-01 13:00:50 +0530

    bitfinex corrected.

--- cryptoTelegramBot_TEST/cryptoTelegramBot_TEST/fetchBitfinex.py
+++ cryptoTelegramBot_TEST/cryptoTelegramBot_TEST/fetchBitfinex.py
@@ -6,7 +6,7 @@ from time import sleep
 
 class FetchBitfinex:
     def __init__(self):
-        print "inside Bitfinex constructor"
+        #print "inside Bitfinex constructor"
         self.allSymbols_link = "https://api.bitfinex.com/v1/symbols"
         self.symbolDetails_link = "https://api.bitfinex.com/v1/pubticker/"
         
@@ -14,42 +14,42 @@ class FetchBitfinex:
         self.db.setup()
         
     def setFetchTime(self):
-        print "setFetchTime -- Bitfinex"
+        #print "setFetchTime -- Bitfinex"
         self.fetchTime = int(time.time())
 
     def setDelTillFetchTime(self):
-        print "delTillFetchTime -- Bitfinex"
+        #print "delTillFetchTime -- Bitfinex"
         #2days older data.
         deleteTime = 172800
 #        deleteTime = 1
         self.delTillFetchTime = self.fetchTime - deleteTime
 
     def fetchData(self):
-        print "fetchData -- Bitfinex"
+        #print "fetchData -- Bitfinex"
         self.f1 = requests.get(url = self.allSymbols_link)
-        print "url set -- Bitfinex"
+        #print "url set -- Bitfinex"
         self.data1 = self.f1.text.replace("null","0")
-        print "replace null -- Bitfinex"
+        #print "replace null -- Bitfinex"
         self.jsonList1  = json.loads(self.data1)
-        print "jsonlist1 -- bitfinex"
+        #print "jsonlist1 -- bitfinex"
         length1 = len(json.loads(self.data1))
-        print "length1 -- bitfinex"
+        #print "length1 -- bitfinex"
         
         for x in range(0,length1):
             try:
-                print "fetchData -- for loop -- Bitfinex"
+                #print "fetchData -- for loop -- Bitfinex"
                 self.f2 = requests.get(url = self.symbolDetails_link + self.jsonList1[x])
-                print "url set 2 -- Bitfinex"
+                #print "url set 2 -- Bitfinex"
                 self.data2 = self.f2.text.replace("null","0")
-                print "DATA -- bitfinex"
-                print self.data2 
-                print "replace null 2 -- Bitfinex"
+                #print "DATA -- bitfinex"
+                #print self.data2 
+                #print "replace null 2 -- Bitfinex"
                 self.jsonList2  = json.loads(self.data2)
-                print "jsonlist2 -- bitfinex"
+                #print "jsonlist2 -- bitfinex"
                 
-                print "marketname --> " +  self.jsonList1[x]
+                #print "marketname --> " +  self.jsonList1[x]
                 self.marketname = self.jsonList1[x]
-                print "mid --> " +  self.jsonList2["mid"]
+                #print "mid --> " +  self.jsonList2["mid"]
                 self.mid = self.jsonList2["mid"]
                 self.bid = self.jsonList2["bid"]
                 self.ask = self.jsonList2["ask"]
@@ -60,22 +60,23 @@ class FetchBitfinex:
                 self.timestamp = self.jsonList2["timestamp"]
                 
                 self.saveIntoDB()
+                #Added delay of 1 second so as to avoid crossing ErrorLimit 
                 sleep(1)
             except Exception as e: 
                     print(e)
-                    print "mid --> " + self.jsonList2["mid"]
+                    #print "mid --> " + self.jsonList2["mid"]
                 
 
     def saveIntoDB(self): 
-        print "saveIntoDB -- Bitfinex"           
+        #print "saveIntoDB -- Bitfinex"           
         self.db.addBitfinex(self.marketname,self.mid,self.bid,self.ask,self.last_price,self.low,self.high,self.volume,self.timestamp,self.fetchTime)
 
     def deleteFromDB_fetchTime(self):
-        print "deleteFromDB_fetchTime -- Bitfinex"
+        #print "deleteFromDB_fetchTime -- Bitfinex"
         self.db.deleteFromDB_fetchTime("bitfinex",self.delTillFetchTime)
     
     def start(self,sleepTime):
-        print "Start method -- Bitfinex"
+        #print "Start method -- Bitfinex"
         self.sleepTime = sleepTime
         try:
             while True:
@@ -88,8 +89,8 @@ class FetchBitfinex:
                 except Exception as e: 
                     print(e)
                     self.sleepTime = 2 * self.sleepTime
-                    print "exception caught in while loop -- Bitfinex"
+                    #print "exception caught in while loop -- Bitfinex"
                 sleep(self.sleepTime)
         except Exception as e: 
             print(e)
-            print "Exception caught in start function -- Bitfinex"
+            #print "Exception caught in start function -- Bitfinex"
